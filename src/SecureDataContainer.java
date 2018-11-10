a122f3d1ec4a2044b68e7956292a81325df229aa
Federico Matteoni <federicomatteoni@live.it>
2018-11-10 11:08:01 +0100

    Interfaccia creata

---
+++ src/SecureDataContainer.java
@@ -0,0 +1,43 @@
+import java.util.Iterator;
+
+/**
+ * Data Storage per la memorizzazione e condivisione sicura di oggetti di tipo E
+ * @author Federico Matteoni
+ * @param <E> il tipo degli oggetti contenuti
+ */
+public interface SecureDataContainer<E> {
+    
+    //Crea l'identità di un nuovo utente della collezione
+    public void createUser(String id, String passw);
+    
+    //Restituisce il numero degli elementi di un utente presenti nella
+    //collezione
+    public int getSize(String owner, String passw);
+    
+    //Inserisce il valore del dato nella collezione se vengono rispettati i
+    //controlli di identità
+    public boolean put(String owner, String passw, E data);
+    
+    //Ottiene una copia del valore del dato nella collezione se vengono
+    //rispettati i controlli di identità
+    public E get(String owner, String passw, E data);
+    
+    //Rimuove il dato dalla collezione se vengono rispettati i controlli di
+    //identità
+    public E remove(String owner, String passw, E data);
+    
+    //Crea una copia del dato nella collezione se vengono rispettati i controlli
+    //di identità
+    public void copy(String owner, String passw, E data);
+    
+    //Condivide il dato nella collezione con un altro utente se vengono
+    //rispettati i controlli di identità
+    public void share(String owner, String passw, String other, E data);
+    
+    //Restituisce un iteratore (senza remove) che genera tutti i dati
+    //dell'utente in ordine arbitrario se vengono rispettati i controlli di
+    //qualità
+    public Iterator<E> getIterator(String owner, String passw);
+    
+    //...altre operazioni da definire a scelta
+}
