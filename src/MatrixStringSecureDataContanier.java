c0f3f82c16c9f921ac4c83bb3d5487265fb47c12
Federico Matteoni <federicomatteoni@live.it>
2018-11-12 12:06:01 +0100

    Pulizia

--- src/MatrixStringSecureDataContanier.java
+++ src/MatrixStringSecureDataContanier.java
@@ -1,69 +1,68 @@
 import java.util.Iterator;
 
 /**
- *
  * @author Federico Matteoni
  */
 public class MatrixStringSecureDataContanier implements SecureDataContainer<String> {
 
     @Override
     public void createUser(String id, String passw) throws InvalidUserException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public int getSize(String owner, String passw) throws UserNotFoundException, InvalidPasswordException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public boolean put(String owner, String passw, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public String get(String owner, String passw, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException, DataNotOwnedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public String remove(String owner, String passw, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException, DataNotOwnedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public void copy(String owner, String passw, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException, DataNotOwnedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public void share(String owner, String passw, String other, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException, DataNotOwnedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public Iterator<String> getIterator(String owner, String passw) throws UserNotFoundException, InvalidPasswordException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public boolean verifyUser(String user, String passw) throws UserNotFoundException, InvalidPasswordException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public boolean verifyOwnership(String user, String passw, String data) throws UserNotFoundException, InvalidPasswordException, InvalidDataException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public int getUsersN() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
 
     @Override
     public int getDataN(String user, String passw) throws UserNotFoundException, InvalidPasswordException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Non supportato.");
     }
     
 }
